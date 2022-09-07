48c7bb4e3c20ab18d15a25df475fae674d492824
Roman Petrenko <roman@canva.com>
2022-09-07 22:32:41 +1000

    The ability to move pipes around

--- pipes/PipePuzzle.gd
+++ pipes/PipePuzzle.gd
@@ -9,7 +9,10 @@ const INIT_FIELD = [
 	[5, 6, 7, 1]
 ]
 
-var field = []
+const field = []
+
+var hole_x = 0
+var hole_y = 0
 
 func _ready():
 	field.resize(INIT_FIELD.size())
@@ -17,7 +20,36 @@ func _ready():
 		field[y] = []
 		field[y].resize(INIT_FIELD[y].size())
 		for x in INIT_FIELD[y].size():
-			if (INIT_FIELD[y][x] != 0):
+			var type = INIT_FIELD[y][x]
+			if (type == 0):
+				hole_x = x
+				hole_y = y
+			else:
 				var tile = PipeTile.instance()
-				tile.setup(x, y, INIT_FIELD[y][x])
+				field[y][x] = tile
+				tile.setup(x, y, type)
+				tile.connect("tile_pressed", self, "_on_tile_pressed")
 				$Board.add_child(tile)
+
+func _on_tile_pressed(x, y):
+	if (x == hole_x):
+		if (y < hole_y):
+			for tile_y in range(hole_y, y, -1):
+				field[tile_y][x] = field[tile_y - 1][x]
+				field[tile_y][x].move_to(x, tile_y)
+		elif (y > hole_y):
+			for tile_y in range(hole_y, y):
+				field[tile_y][x] = field[tile_y + 1][x]
+				field[tile_y][x].move_to(x, tile_y)
+	elif (y == hole_y):
+		if (x < hole_x):
+			for tile_x in range(hole_x, x, -1):
+				field[y][tile_x] = field[y][tile_x - 1]
+				field[y][tile_x].move_to(tile_x, y)
+		elif (x > hole_x):
+			for tile_x in range(hole_x, x):
+				field[y][tile_x] = field[y][tile_x + 1]
+				field[y][tile_x].move_to(tile_x, y)
+	field[y][x] = null
+	hole_x = x
+	hole_y = y
--- pipes/PipeTile.gd
+++ pipes/PipeTile.gd
@@ -2,6 +2,9 @@ extends TextureButton
 
 signal tile_pressed
 
+var x: int
+var y: int
+
 # Class Constructor
 func setup(x: int, y: int, type: int):
 	match type:
@@ -23,9 +26,16 @@ func setup(x: int, y: int, type: int):
 			$Sprite.rotation_degrees = 270
 		7:
 			$Sprite.texture = load("res://images/pipe_crossed.png")
+	self.x = x
+	self.y = y
 	rect_position.x = rect_size.x * x
 	rect_position.y = rect_size.y * y
 
+func move_to(x: int, y: int):
+	self.x = x
+	self.y = y
+	rect_position.x = rect_size.x * x
+	rect_position.y = rect_size.y * y
 
 func _on_Tile_pressed():
-	emit_signal("tile_pressed")
+	emit_signal("tile_pressed", x, y)
