e3b0df949ef495b19ebffe857a7a685fc60e4632
Roman Petrenko <roman@canva.com>
2022-09-11 11:21:39 +1000

    Paint pipes from all entrances

--- pipes2/PipePuzzle.gd
+++ pipes2/PipePuzzle.gd
@@ -52,14 +52,41 @@ func repaint_pipes():
 			for layer in field[y][x].layers:
 				layer.color = PipeColor.NEUTRAL
 	paint_pipe(PipeColor.YELLOW, 0, 0, Direction.UP)
+	paint_pipe(PipeColor.BLUE, 2, 0, Direction.UP)
+	paint_pipe(PipeColor.YELLOW, 1, 3, Direction.DOWN)
+	paint_pipe(PipeColor.BLUE, 0, 3, Direction.DOWN)
 	for y in field.size():
 		for x in field[y].size():
 			field[y][x].repaint_layers()
 
 func paint_pipe(color: int, x: int, y: int, direction: int):
 	for layer in field[y][x].layers:
-		if (layer.start == direction || layer.end == direction):
+		if (layer.start == direction):
 			layer.color = color
+			find_and_paint_next_pipe(color, x, y, layer.end)
+		elif (layer.end == direction):
+			layer.color = color
+			find_and_paint_next_pipe(color, x, y, layer.start)
+
+func find_and_paint_next_pipe(color: int, x: int, y: int, direction: int):
+	var next_x = x
+	var next_y = y
+	var next_direction = direction
+	match direction:
+		Direction.UP:
+			next_y -= 1
+			next_direction = Direction.DOWN
+		Direction.DOWN:
+			next_y += 1
+			next_direction = Direction.UP
+		Direction.LEFT:
+			next_x -= 1
+			next_direction = Direction.RIGHT
+		Direction.RIGHT:
+			next_x += 1
+			next_direction = Direction.LEFT
+	if (next_x >= 0 && next_y >= 0 && next_x < field[y].size() && next_y < field.size()):
+		paint_pipe(color, next_x, next_y, next_direction)
 
 func on_move_completed():
 	repaint_pipes()
